212df0f42aca258852cf8887f4e9b6b9b083782a
maddie <lee.maddie@gmail.com>
2023-08-29 17:09:31 -0400

    First pass at implementing function - unsuccessful

--- eqObjects.js
+++ eqObjects.js
@@ -10,13 +10,26 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  // default return is false
+  let match = false;
+
+  for (let i = 0; i < keys1.length; i++) {
+    if (keys1[i] === keys2[i] && object1[keys1] === object2[keys2]) {
+      match = true;
+      // will only stay true if all objects match
+    }
+  }
+
+  return match;
 };
 
 // TEST CODE
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
+const anotherShirtObject = { size: "medium", color: "red" };
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
