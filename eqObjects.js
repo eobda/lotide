f64a0798c1b136ed896751cfc1da322810125e24
maddie <lee.maddie@gmail.com>
2023-08-29 17:12:53 -0400

    Updated code so it works with objects of differing length

--- eqObjects.js
+++ eqObjects.js
@@ -10,15 +10,30 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+  // keys should be sorted so it doesn't matter what order they appear in the original object
+  const keys1 = Object.keys(object1).sort();
+  const keys2 = Object.keys(object2).sort();
+
   // default return is false
   let match = false;
 
-  for (let i = 0; i < keys1.length; i++) {
-    if (keys1[i] === keys2[i] && object1[keys1] === object2[keys2]) {
-      match = true;
-      // will only stay true if all objects match
+  if (keys1.length >= keys2.length) {
+    for (let i = 0; i < keys1.length; i++) {
+      if (keys1[i] === keys2[i] && object1[keys1] === object2[keys2]) {
+        match = true;
+        // will only stay true if all objects match
+      } else {
+        match = false;
+      }
+    }
+  } else {
+    for (let i = 0; i < keys2.length; i++) {
+      if (keys1[i] === keys2[i] && object1[keys1] === object2[keys2]) {
+        match = true;
+        // will only stay true if all objects match
+      } else {
+        match = false;
+      }
     }
   }
 
