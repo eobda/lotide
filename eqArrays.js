0e13f883ec5244429831ba9bca1eef25a110bfc0
maddie <lee.maddie@gmail.com>
2023-08-30 10:44:16 -0400

    Refactored code to eliminate use of local variable

--- eqArrays.js
+++ eqArrays.js
@@ -12,24 +12,21 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   // default return value is false in case arrays are not of matching length
-  let arrayMatch = false;
-
-  if (arr1.length === arr2.length) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
     for (let i = 0; i < arr1.length; i++) {
       // compares each element of array 1 to its corresponding element in array 2
-      if (arr1[i] === arr2[i]) {
-        arrayMatch = true;
-      } else {
-        // resets return value to false if any elements are not matching
-        arrayMatch = false;
+      if (arr1[i] !== arr2[i]) {
+        return false;
       }
     }
-  }
 
-  return arrayMatch;
+    return true;
+  }
 }
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
