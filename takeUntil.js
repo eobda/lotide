5a60a10b718e18059a040a92b42cc7acb7ec6c69
maddie <lee.maddie@gmail.com>
2023-09-02 15:07:12 -0400

    Base code for takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,14 @@
+const takeUntil = function(array,  callback) {
+
+};
+
+// test code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); // expected output: [ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2); // expected output: [ 'I've', 'been', 'to', 'Hollywood' ]
