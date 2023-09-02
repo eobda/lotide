e0355972990bf8a2b534895bf2314e0879db5d5d
maddie <lee.maddie@gmail.com>
2023-09-02 15:12:29 -0400

    Implemented function that outputs a slice of an array until the callback returns a truthy value

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,16 @@
-const takeUntil = function(array,  callback) {
+const takeUntil = function(array, callback) {
+  // should output a fresh array
+  let result = [];
 
+  for (const element of array) {
+    // once the callback is truthy, stop running the code and return the result
+    if (callback(element)) {
+      return result;
+    } else {
+      // add the elements of the array to the result while the callback result is not truthy
+      result.push(element);
+    }
+  }
 };
 
 // test code
