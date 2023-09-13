2eec0402dc669890eaa51c326931a34aeed33f88
maddie <lee.maddie@gmail.com>
2023-09-13 10:01:18 -0400

    Refactored code using exports. Separated test code

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(4, "apple");
-assertEqual("apple", "apple");
-assertEqual(2 * 2, 4);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(4, "apple");
+assertEqual("apple", "apple");
+assertEqual(2 * 2, 4);
